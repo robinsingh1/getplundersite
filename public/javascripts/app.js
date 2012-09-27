7e701e536563ff117b2db9d45658867a312a04a2
Robin Singh <robin.singh.1991@gmail.com>
2012-09-27 13:26:22 -0400

    added bitly refer link data

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,46 +1,36 @@
-/*
-  window.fbAsyncInit = function() {
-        FB.init({
-          appId      : "<%= @app['id'] %>",                     // App ID
-          channelUrl : "<%= url_no_scheme('/channel.html') %>", // Channel File
-          status     : true,                                    // check login status
-          cookie     : true,                                    // enable cookies
-          xfbml      : true                                     // parse XFBML
-        });
+$(window).resize(function(){
+    console.log($(window).width())
+    console.log($(window).height())
+});
 
-        FB.Event.subscribe('auth.login', function(response) {
-          window.location = window.location;
-        });
+function getShortUrl(longUrl, login, apiKey, func)
+{
+    $.getJSON(
+        "http://api.bitly.com/v3/shorten?callback=?", 
+        { 
+            "format": "json",
+            "apiKey": api_key,
+            "login": login,
+            "longUrl": long_url
+        },
+        function(response)
+        {
+            func(response.data.url);
+        }
+    );
+}
 
-        FB.Canvas.setAutoGrow();
+Parse.initialize("eBWdI7PNUqyKjVScceC1KfKIRw3N1ScfOM45JOBE", 
+                "6bQSQ8KlG8aJ8qJXOwrMbP6eRt7xeZZyW2moKfuP");
 
-        FB.getLoginStatus(function(response) {
-            if (response.status === 'connected') {
-                var uid = response.authResponse.userID;
-                var accessToken = response.authResponse.accessToken;
-                console.log('logged in')
-                $('#infoForm').hide()
-                $('#share').show()
-            } else if (response.status === 'not_authorized') {
-                // the user is logged in to Facebook, 
-                // but has not authenticated your app
-                console.log('not authorized')
-            } else {
-                // the user isn't logged in to Facebook.
-                console.log('not logged in')
-            }
-        });
-      };
-*/
+var Member = Parse.Object.extend("Members");
 
-   Parse.initialize("eBWdI7PNUqyKjVScceC1KfKIRw3N1ScfOM45JOBE", "6bQSQ8KlG8aJ8qJXOwrMbP6eRt7xeZZyW2moKfuP");
-
-    var Member = Parse.Object.extend("Members");
-
-  function the_login(){
+function the_login(){
      FB.login(function(response) {
        if (response.authResponse) {
          console.log('Welcome!  Fetching your information.... ');
+         url = getShortUrl('www.getplunder.com','o_3n1puoqkjk',
+                            'R_0877992b078175ddd6477ef109a01edd');
          FB.api('/me', function(r) {
             var member = new Member();
             member.save({
@@ -50,6 +40,7 @@
                 email: r.email,
                 password: 'lol',
                 country:r.locale,
+                referURL:url,
                 facebookToken: FB.getAuthResponse()['accessToken'],
             },{
             success: function(object) {
@@ -71,32 +62,37 @@
      });
     }
     
-    function userSave(){
-        var member = new Member();
-        member.save({
-                firstName: $('#firstName').val(),
-                lastName:  $('#lastName').val(),
-                gender:  $('#gender').val(),
-                password:  $('#password').val(),
-                country:  $('#country').val(),
-                email:  $('#email').val(),
-                facebookToken: '',
-            },{
-            success: function(object){
-                console.log("success")
-            },
-            error: function(model,error) {
-                console.log("error")
-            }
-        });
-    }
-/*
-      // Load the SDK Asynchronously
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/all.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-*/
+function userSave(){
+    var member = new Member();
+    url = getShortUrl('www.getplunder.com','o_3n1puoqkjk',
+                        'R_0877992b078175ddd6477ef109a01edd');
+    member.save({
+            firstName: $('#firstName').val(),
+            lastName:  $('#lastName').val(),
+            gender:  $('#gender').val(),
+            password:  $('#password').val(),
+            country:  $('#country').val(),
+            email:  $('#email').val(),
+            referURL: url,
+            facebookToken: '',
+        },{
+        success: function(object){
+            console.log("success")
+        },
+        error: function(model,error) {
+            console.log("error")
+        }
+    });
+}
+
+function userURL(){
+    var query = new Parse.Query('Members')
+    query.equalTo("lastName", "Singh");
+    query.find({
+        success: function(results){
+            console.log("query success")
+        },
+        error: function(results){
+            console.log("query error")
+        }
+}
